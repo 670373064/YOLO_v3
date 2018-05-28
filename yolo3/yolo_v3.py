7e937f0f913f3c7a00e0bef2c2c7de271f6cfb84
leeyoshinari <lee_jc@outlook.com>
2018-05-28 12:54:57 +0800

    Update yolo_v3.py

--- yolo3/yolo_v3.py
+++ yolo3/yolo_v3.py
@@ -2,7 +2,7 @@
 #
 # Written by leeyoshinari
 #
-#2018-04-21
+# 2018-04-21
 
 import tensorflow as tf
 import numpy as np
@@ -30,9 +30,7 @@ class yolo_v3(object):
         self.logits = self.yolo_net(self.images)
 
         if is_training:
-            self.label1 = tf.placeholder(tf.float32, [self.batch_size, self.cell_size, self.cell_size, self.box_per_cell, self.num_class+5])
-            self.label2 = tf.placeholder(tf.float32, [self.batch_size, self.cell_size*2, self.cell_size*2, self.box_per_cell, self.num_class + 5])
-            self.label3 = tf.placeholder(tf.float32, [self.batch_size, self.cell_size*4, self.cell_size*4, self.box_per_cell, self.num_class + 5])
+            self.labels = tf.placeholder(tf.float32, [self.batch_size, 5])
             self.total_loss = self.calc_loss(self.logits, [self.label1, self.label2, self.label3])
             tf.summary.scalar('total_loss', self.total_loss)
         
@@ -43,99 +41,99 @@ class yolo_v3(object):
         #short_cut = net
         net = self.conv_layer(net, 1, 32, idx = 3)  #2
         net = self.conv_layer(net, 3, 64, idx = 4)  #3
-        net = tf.add(short_cut, net)  #4
+        net = short_cut + net  #4
 
         short_cut = net = self.conv_layer(net, 3, 128, stride = 2, idx = 5)  #5
         net = self.conv_layer(net, 1, 64, idx = 6)  #6
         net = self.conv_layer(net, 3, 128, idx = 7)  #7
-        short_cut = net = tf.add(short_cut, net)  #8
+        short_cut = net = short_cut + net  #8
 
         net = self.conv_layer(net, 1, 64, idx = 8)  #9
         net = self.conv_layer(net, 3, 128, idx = 9)  #10
-        net = tf.add(short_cut, net)  #11
+        net = short_cut + net  #11
 
         short_cut = net = self.conv_layer(net, 3, 256, stride = 2, idx = 10)  #12
         net = self.conv_layer(net, 1, 128, idx = 11)  #13
         net = self.conv_layer(net, 3, 256, idx = 12)  #14
-        short_cut = net = tf.add(short_cut, net)  #15
+        short_cut = net = short_cut + net  #15
 
         net = self.conv_layer(net, 1, 128, idx = 13)  #16
         net = self.conv_layer(net, 3, 256, idx = 14)  #16
-        short_cut = net = tf.add(short_cut, net)  #18
+        short_cut = net = short_cut + net  #18
 
         net = self.conv_layer(net, 1, 128, idx = 15)  #19
         net = self.conv_layer(net, 3, 256, idx = 16)  #20
-        short_cut = net = tf.add(short_cut, net)  #21
+        short_cut = net = short_cut + net  #21
 
         net = self.conv_layer(net, 1, 128, idx = 17)  #22
         net = self.conv_layer(net, 3, 256, idx = 18)  #23
-        short_cut = net = tf.add(short_cut, net)  #24
+        short_cut = net = short_cut + net  #24
 
         net = self.conv_layer(net, 1, 128, idx = 19)  #25
         net = self.conv_layer(net, 3, 256, idx = 20)  #26
-        short_cut = net = tf.add(short_cut, net)  #27
+        short_cut = net = short_cut + net  #27
 
         net = self.conv_layer(net, 1, 128, idx = 21)  #28
         net = self.conv_layer(net, 3, 256, idx = 22)  #29
-        short_cut = net = tf.add(short_cut, net)  #30
+        short_cut = net = short_cut + net  #30
 
         net = self.conv_layer(net, 1, 128, idx = 23)  #31
         net = self.conv_layer(net, 3, 256, idx = 24)  #32
-        short_cut = net = tf.add(short_cut, net)  #33
+        short_cut = net = short_cut + net  #33
 
         net = self.conv_layer(net, 1, 128, idx = 25)  #34
         net = self.conv_layer(net, 3, 256, idx = 26)  #35
-        net36 = tf.add(short_cut, net)  #36
+        net36 = short_cut + net  #36
 
         short_cut = net = self.conv_layer(net36, 3, 512, stride = 2, idx = 27)  #37
         net = self.conv_layer(net, 1, 256, idx = 28)  #38
         net = self.conv_layer(net, 3, 512, idx = 29)  #39
-        short_cut = net = tf.add(short_cut, net)  #40
+        short_cut = net = short_cut + net  #40
 
         net = self.conv_layer(net, 1, 256, idx = 30)  #41
         net = self.conv_layer(net, 3, 512, idx = 31)  #42
-        short_cut = net = tf.add(short_cut, net)  #43
+        short_cut = net = short_cut + net  #43
 
         net = self.conv_layer(net, 1, 256, idx = 32)  #44
         net = self.conv_layer(net, 3, 512, idx = 33)  #45
-        short_cut = net = tf.add(short_cut, net)  #46
+        short_cut = net = short_cut + net  #46
 
         net = self.conv_layer(net, 1, 256, idx = 34)  #47
         net = self.conv_layer(net, 3, 512, idx = 35)  #48
-        short_cut = net = tf.add(short_cut, net)  #49
+        short_cut = net = short_cut + net  #49
 
         net = self.conv_layer(net, 1, 256, idx = 36)  #50
         net = self.conv_layer(net, 3, 512, idx = 37)  #51
-        short_cut = net = tf.add(short_cut, net)  #52
+        short_cut = net = short_cut + net  #52
 
         net = self.conv_layer(net, 1, 256, idx = 38)  #53
         net = self.conv_layer(net, 3, 512, idx = 39)  #54
-        short_cut = net = tf.add(short_cut, net)  #55
+        short_cut = net = short_cut + net  #55
 
         net = self.conv_layer(net, 1, 256, idx = 40)  #56
         net = self.conv_layer(net, 3, 512, idx = 41)  #57
-        short_cut = net = tf.add(short_cut, net)  #58
+        short_cut = net = short_cut + net  #58
 
         net = self.conv_layer(net, 1, 256, idx = 42)  #59
         net = self.conv_layer(net, 3, 512, idx = 43)  #60
-        net61 = tf.add(short_cut, net)  #61
+        net61 = short_cut + net  #61
 
         short_cut = net = self.conv_layer(net61, 3, 1024, stride = 2, idx = 44)  #62
         net = self.conv_layer(net, 1, 512, idx = 45)  #63
         net = self.conv_layer(net, 3, 1024, idx = 46)  #64
-        short_cut = net = tf.add(short_cut, net)  #65
+        short_cut = net = short_cut + net  #65
 
         net = self.conv_layer(net, 1, 512, idx = 47)  #66
         net = self.conv_layer(net, 3, 1024, idx = 48)  #67
-        short_cut = net = tf.add(short_cut, net)  #68
+        short_cut = net = short_cut + net  #68
 
         net = self.conv_layer(net, 1, 512, idx = 49)  #69
         net = self.conv_layer(net, 3, 1024, idx = 50)  #70
-        short_cut = net = tf.add(short_cut, net)  #71
+        short_cut = net = short_cut + net  #71
 
         net = self.conv_layer(net, 1, 512, idx = 51)  #72
         net = self.conv_layer(net, 3, 1024, idx = 52)  #73
-        net = tf.add(short_cut, net)  #74
+        net = short_cut + net  #74
 
         net = self.conv_layer(net, 1, 512, idx = 53)  #75
         net = self.conv_layer(net, 3, 1024, idx = 54)  #76
@@ -147,7 +145,7 @@ class yolo_v3(object):
 
         net = self.conv_layer(net57, 1, 256, idx = 60)  #84
         net = self.upsampling(net)  #85
-        net = tf.concat([net, net61], axis = 3)  #86f
+        net = tf.concat([net, net61], axis = -1)  #86f
 
         net = self.conv_layer(net, 1, 256, idx = 61)  #87
         net = self.conv_layer(net, 3, 512, idx = 62)  #88
@@ -159,7 +157,7 @@ class yolo_v3(object):
 
         net = self.conv_layer(net65, 1, 128, idx = 68)  #96
         net = self.upsampling(net)  #97
-        net = tf.concat([net, net36], axis = 3) #98
+        net = tf.concat([net, net36], axis = -1) #98
 
         net = self.conv_layer(net, 1, 128, idx = 69)  #99
         net = self.conv_layer(net, 3, 256, idx = 70)  #100
@@ -176,25 +174,24 @@ class yolo_v3(object):
         with tf.variable_scope('convolutional'+str(idx)):
             channel = int(input.shape[3])
             weight = tf.Variable(tf.truncated_normal([filter, filter, channel, size], stddev=0.1), name='weights')
-            biases = tf.Variable(tf.constant(0.1, shape=[size]), name='biases')
+            #biases = tf.Variable(tf.constant(0.1, shape=[size]), name='biases')
             conv = tf.nn.conv2d(input, weight, strides=[1, stride, stride, 1], padding='SAME')
 
             if batch_norm:
                 with tf.variable_scope('BatchNorm'):
                     depth = conv.shape[3]
                     betas = tf.Variable(tf.ones([depth, ], dtype='float32'), name='beta')
-                    shift = tf.Variable(tf.zeros([depth, ], dtype='float32'), name='shift')
+                    shift = tf.Variable(tf.zeros([depth, ], dtype='float32'), name='biases')
                     mean = tf.Variable(tf.ones([depth, ], dtype='float32'), name='moving_mean')
                     variance = tf.Variable(tf.ones([depth, ], dtype='float32'), name='moving_variance')
 
                     conv = tf.nn.batch_normalization(conv, mean, variance, shift, betas, 1e-05, name='BatchNorm')
 
-            conv = tf.add(conv, biases)
-
-        if activation:
-            return tf.maximum(self.alpha * conv, conv)
-        else:
-            return conv
+            #conv = tf.add(conv, biases)
+            if activation:
+                return tf.maximum(self.alpha * conv, conv)
+            else:
+                return conv
 
 
     def upsampling(self, input, size = 2):
@@ -209,6 +206,20 @@ class yolo_v3(object):
         return net_transfer
 
 
+    def leaky_ReLU(self, input):
+        return tf.maximum(input, input * self.alpha)
+
+    def process_label(self, label, cell_size):
+        labels = tf.zeros([self.batch_size, cell_size, cell_size, self.box_per_cell, 5+self.num_class])
+        for i in range(self.batch_size):
+            x_ind = int(tf.floor(1.0 * label[i][0] * cell_size))
+            y_ind = int(tf.floor(1.0 * label[i][1] * cell_size))
+            labels[i, :, :, :, 0] = 1
+            labels[i, :, :, :, 1:5] = label[i][0:4]
+            labels[i, :, :, :, 5+label[i][4]] = 1
+        return labels
+
+
     def loss_layer(self, predict, labels, anchor):
         grid_h, grid_w = predict.shape[1:3]
         grid_h, grid_w = int(grid_h), int(grid_w)
@@ -216,56 +227,66 @@ class yolo_v3(object):
         row = np.reshape(np.tile(np.reshape(np.arange(0, grid_h), [-1, 1]), grid_h), [1, grid_h, grid_w, 1])
         col = np.tile(col, (self.batch_size, 1, 1, 3))
         row = np.tile(row, (self.batch_size, 1, 1, 3))
+        offset = np.transpose(np.reshape(np.array([np.arange(grid_w)] * grid_h * self.box_per_cell),
+                                         [self.box_per_cell, grid_w, grid_h]), (1, 2, 0))
+        offset = tf.reshape(tf.constant(offset, dtype=tf.float32),
+                            [1, grid_w, grid_h, self.box_per_cell])
+        offset = tf.tile(offset, (self.batch_size, 1, 1, 1))
 
         predict = tf.reshape(predict, [self.batch_size, grid_h, grid_w, self.box_per_cell, self.num_class+5])
         box_coordinate = tf.reshape(predict[:, :, :, :, :4], [self.batch_size, grid_h, grid_w, self.box_per_cell, 4])
         box_confidence = tf.reshape(predict[:, :, :, :, 4], [self.batch_size, grid_h, grid_w, self.box_per_cell, 1])
         box_classes = tf.reshape(predict[:, :, :, :, 5:], [self.batch_size, grid_h, grid_w, self.box_per_cell, self.num_class])
 
-        boxes1 = tf.stack([(1.0 / (1.0 + tf.exp(-1.0 * box_coordinate[:, :, :, :, 0])) + col) / grid_w,
-                           (1.0 / (1.0 + tf.exp(-1.0 * box_coordinate[:, :, :, :, 1])) + row) / grid_h,
-                           tf.exp(box_coordinate[:, :, :, :, 2]) * anchor[:, 0] / self.image_size,
-                           tf.exp(box_coordinate[:, :, :, :, 3]) * anchor[:, 1] / self.image_size])
-        box_coor_trans = tf.transpose(boxes1, (1, 2, 3, 4, 0)) * self.image_size
+        anchor_w = np.reshape(anchor[:, 0], [1, 1, 1, 3]) / self.image_size
+        anchor_h = np.reshape(anchor[:, 1], [1, 1, 1, 3]) / self.image_size
+
+        boxes1 = tf.stack([(1.0 / (1.0 + tf.exp(-1.0 * box_coordinate[:, :, :, :, 0])) + offset) / grid_w,
+                           (1.0 / (1.0 + tf.exp(-1.0 * box_coordinate[:, :, :, :, 1])) + tf.transpose(offset, (0, 2, 1, 3))) / grid_h,
+                           tf.sqrt(tf.exp(box_coordinate[:, :, :, :, 2]) * anchor_w),
+                           tf.sqrt(tf.exp(box_coordinate[:, :, :, :, 3]) * anchor_h)])
+        box_coor_trans = tf.transpose(boxes1, (1, 2, 3, 4, 0))
         box_confidence = 1.0 / (1.0 + tf.exp(-1.0 * box_confidence))
-        #box_confidence = tf.tile(tf.expand_dims(box_confidence, axis = 4), (1, 1, 1, 1, self.num_class))
         box_classes = 1.0 / (1.0 + tf.exp(-1.0 * box_classes))
 
+        labels = self.process_label(labels, grid_h)
         response = tf.reshape(labels[:, :, :, :, 0], [self.batch_size, grid_h, grid_w, self.box_per_cell])
         boxes = tf.reshape(labels[:, :, :, :, 1:5], [self.batch_size, grid_h, grid_w, self.box_per_cell, 4])
         classes = tf.reshape(labels[:, :, :, :, 5:], [self.batch_size, grid_h, grid_w, self.box_per_cell, self.num_class])
 
-        iou = self.calc_iou(box_coor_trans, boxes)
-        best_box = tf.to_float(tf.equal(iou, tf.reduce_max(iou, [2], True)))
+        iou = self.calc_iou(box_coordinate, boxes)
+        best_box = tf.reduce_max(iou, axis=-1, keep_dims=True)
+        best_box = tf.cast(best_box > 0.5, dtype=best_box.dtype)
         confs = tf.expand_dims(best_box * response, axis = 4)
 
         conid = self.noobject_scale * (1.0 - confs) + self.object_scale * confs
-        cooid = self.coordinate_scale * tf.concat(4 * [confs], 4)
-        proid = self.class_scale * tf.concat(self.num_class * [confs], 4)
+        cooid = self.coordinate_scale * confs
+        proid = self.class_scale * confs
 
         coo_loss = cooid * tf.square(box_coor_trans - boxes)
         con_loss = conid * tf.square(box_confidence - confs)
         pro_loss = proid * tf.square(box_classes - classes)
 
         loss = tf.concat([coo_loss, con_loss, pro_loss], axis=4)
-        loss = tf.reshape(loss, [self.batch_size, grid_h * grid_w * self.box_per_cell * (self.num_class + 5)])
-        loss = 0.5 * tf.reduce_mean(tf.reduce_mean(loss, axis=1))
+        loss = tf.reduce_mean(tf.reduce_sum(loss, axis=[1, 2, 3, 4]), name='loss')
 
         return loss
 
     def calc_iou(self, boxes1, boxes2):
-        boxes1_square = boxes1[:, :, :, :, 2] * boxes1[:, :, :, :, 3]
-        box = tf.stack([boxes1[:, :, :, :, 0] - boxes1[:, :, :, :, 2] * 0.5,
-                        boxes1[:, :, :, :, 1] - boxes1[:, :, :, :, 3] * 0.5,
-                        boxes1[:, :, :, :, 0] + boxes1[:, :, :, :, 2] * 0.5,
-                        boxes1[:, :, :, :, 1] + boxes1[:, :, :, :, 3] * 0.5])
+        boxx = tf.square(boxes1[:, :, :, :, 2:4])
+        boxes1_square = boxx[:, :, :, :, 0] * boxx[:, :, :, :, 1]
+        box = tf.stack([boxes1[:, :, :, :, 0] - boxx[:, :, :, :, 0] * 0.5,
+                        boxes1[:, :, :, :, 1] - boxx[:, :, :, :, 1] * 0.5,
+                        boxes1[:, :, :, :, 0] + boxx[:, :, :, :, 0] * 0.5,
+                        boxes1[:, :, :, :, 1] + boxx[:, :, :, :, 1] * 0.5])
         boxes1 = tf.transpose(box, (1, 2, 3, 4, 0))
 
-        boxes2_square = boxes2[:, :, :, :, 2] * boxes2[:, :, :, :, 3]
-        box = tf.stack([boxes2[:, :, :, :, 0] - boxes2[:, :, :, :, 2] * 0.5,
-                        boxes2[:, :, :, :, 1] - boxes2[:, :, :, :, 3] * 0.5,
-                        boxes2[:, :, :, :, 0] + boxes2[:, :, :, :, 2] * 0.5,
-                        boxes2[:, :, :, :, 1] + boxes2[:, :, :, :, 3] * 0.5])
+        boxx = tf.square(boxes2[:, :, :, :, 2:4])
+        boxes2_square = boxx[:, :, :, :, 0] * boxx[:, :, :, :, 1]
+        box = tf.stack([boxes2[:, :, :, :, 0] - boxx[:, :, :, :, 0] * 0.5,
+                        boxes2[:, :, :, :, 1] - boxx[:, :, :, :, 1] * 0.5,
+                        boxes2[:, :, :, :, 0] + boxx[:, :, :, :, 0] * 0.5,
+                        boxes2[:, :, :, :, 1] + boxx[:, :, :, :, 1] * 0.5])
         boxes2 = tf.transpose(box, (1, 2, 3, 4, 0))
 
         left_up = tf.maximum(boxes1[:, :, :, :, :2], boxes2[:, :, :, :, :2])
